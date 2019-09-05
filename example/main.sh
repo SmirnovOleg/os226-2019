1a9177b4c6c23ce680740750359edb14d992fbad
Oleg <sm-oleg1209@yandex.ru>
2019-09-05 12:35:28 +0300

    Task01 with bash script added

--- example/main.sh
+++ example/main.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
+while read student_id; do
+    let logbook[student_id]+=1;
+done
+
+most_common_id=0
+most_common_times=0
+
+for student_id in ${!logbook[@]}; do
+    if (( ${logbook[student_id]} > $most_common_times )); then
+        most_common_times=${logbook[student_id]}
+        most_common_id=$student_id
+    fi
+done
+
+echo $most_common_id
