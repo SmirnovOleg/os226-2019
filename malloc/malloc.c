abb7cf9134217630b3cc120438ea5b5b0edb6910
Oleg <sm-oleg1209@yandex.ru>
2019-09-22 12:58:59 +0300

    Initial allocator implementation added

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -2,111 +2,173 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <dlfcn.h>
 
 #define CONCAT(...) __VA_ARGS__
 
-#if 0
 #define FNX(_ret, _name, _decl, _call, ...) \
 	static _ret (*_name ## _p)(_decl); \
 	void _name ## _init(void) __attribute__((constructor)); \
 	void _name ## _init(void) { \
 		_name ## _p = dlsym(RTLD_NEXT, # _name); \
-		fprintf(stderr, "init " # _name " %p\n", _name ## _p); \
 	} \
         _ret _name (_decl) { \
 		__VA_ARGS__ \
-                return _name ## _p(_call); \
         }
+
+typedef struct block
+{
+	void *addr;
+	struct block *next;
+} memory_block;
+
+#define LEN(arr) (sizeof(arr)/sizeof(arr[0]))
+#define MEMORY_SIZE (int) 1e9
+
+char main_memory[MEMORY_SIZE];
+size_t block_sizes[] = {10, 30, 120, 240, 500, 1024, 5000, 33000};
+memory_block *free_lists[LEN(block_sizes)];
+char was_initialized = 0;
+void *base = main_memory;
+
+
+static __attribute__((constructor)) void _free_lists_init()
+{
+	int region_size = MEMORY_SIZE / LEN(block_sizes);
+	int index = 0;
+
+	for (void *region_ptr = base;
+			region_ptr < base + MEMORY_SIZE && index < LEN(block_sizes);
+			region_ptr += region_size) 
+	{
+		size_t real_block_size = block_sizes[index] + sizeof(memory_block);
+		free_lists[index++] = (memory_block *) region_ptr;
+
+		for (void *block_ptr = region_ptr;
+			block_ptr < region_ptr + region_size;
+			block_ptr += real_block_size)
+		{
+			memory_block *new_block = (memory_block *) block_ptr;
+			new_block->addr = block_ptr + sizeof(memory_block);
+			if (block_ptr + real_block_size >= region_ptr + region_size)
+				new_block->next = NULL;
+			else
+				new_block->next = (memory_block *) (block_ptr + real_block_size);
+		}
+	}
+	was_initialized = 1;
+}
+
+
+size_t _index_of_approximate(size_t size) {
+	size_t left_index = 0;
+	size_t right_index = LEN(block_sizes) - 1;
+	if (size > block_sizes[right_index]) 
+	{
+		fprintf(stderr, "Allocation error: not enough memory\n");
+		exit(1);
+	}
+	if (size < block_sizes[left_index]) 
+	{
+		return left_index;
+	}
+	while (right_index > left_index + 1) 
+	{
+		int ind_avg = (right_index + left_index) >> 1;
+		if (block_sizes[ind_avg] < size) 
+			left_index = ind_avg;
+		else 
+			right_index = ind_avg;
+	}
+	if (block_sizes[left_index] == size)
+		return left_index;
+	else
+		return right_index;	 
+}
+
 
 FNX(void *, malloc,
 		size_t size,
 		size,
 {
-	fprintf(stderr, "%s size %u\n", __func__, size);
+	if (!was_initialized)
+		_free_lists_init();
+	//fprintf(stderr, "%s size %u\n", __func__, size);
+
+	size_t index = _index_of_approximate(size);
+	memory_block *free_block = free_lists[index];
+	if (free_block == NULL) 
+	{
+		fprintf(stderr, "Allocation error: not enough memory\n");
+		exit(1);
+	}
+	free_lists[index] = free_block->next;
+	return free_block->addr;
 })
 
 FNX(void, free,
 		void *ptr,
 		ptr,
 {
-	fprintf(stderr, "%s ptr %p\n", __func__, ptr);
+	if (!was_initialized)
+		_free_lists_init();
+	//fprintf(stderr, "%s ptr %p\n", __func__, ptr);
+
+	if (ptr == NULL || 
+			!(base + sizeof(memory_block) < ptr 
+				&& ptr < base + MEMORY_SIZE))
+		return;
+
+	memory_block *current_block = (memory_block *) (ptr - sizeof(memory_block));
+	size_t current_block_size = ((size_t) current_block->next) - ((size_t) current_block->addr);
+	size_t index = _index_of_approximate(current_block_size);
+	memory_block *head_block = free_lists[index];
+
+	current_block->addr = ptr;
+	current_block->next = head_block;
+	free_lists[index] = current_block;
 })
 
 FNX(void *, calloc,
 		CONCAT(size_t nmemb, size_t size),
 		CONCAT(nmemb, size),
 {
-	fprintf(stderr, "%s nmemb %u size %u\n", __func__, nmemb, size);
+	if (!was_initialized)
+		_free_lists_init();
+	//fprintf(stderr, "%s nmemb %u size %u\n", __func__, nmemb, size);
+	
+	uint8_t block_size_index = _index_of_approximate(size);
+	memory_block *free_block = free_lists[block_size_index];
+	free_lists[block_size_index] = free_block->next;
+
+	void *current_addr = free_block->addr;
+	for (size_t *ptr = current_addr; ptr < (size_t *) (current_addr + size); ++ptr)
+		*ptr = nmemb;
+
+	return free_block->addr;
 })
 
 FNX(void *, realloc,
 		CONCAT(void *ptr, size_t size),
 		CONCAT(ptr, size),
 {
-	fprintf(stderr, "%s ptr %p size %u\n", __func__, ptr, size);
+	if (!was_initialized)
+		_free_lists_init();
+	//fprintf(stderr, "%s ptr %p size %u\n", __func__, ptr, size);
+
+	free(ptr);
+	return malloc(size);
 })
 
 FNX(void *, reallocarray,
 		CONCAT(void *ptr, size_t nmemb, size_t size),
 		CONCAT(ptr, nmemb, size),
 {
-	fprintf(stderr, "%s ptr %p nmemb %u size %u\n", __func__, ptr, nmemb, size);
+	if (!was_initialized)
+		_free_lists_init();
+	//fprintf(stderr, "%s ptr %p nmemb %u size %u\n", __func__, ptr, nmemb, size);
+
+	free(ptr);
+	return calloc(nmemb, size);
 })
-
-#else
-
-#define MALLOCFN_X(x) \
-        x(void *, malloc, 1, size_t, size, "%u") \
-	x(void, free, 1, void *, ptr, "%p") \
-	x(void *, calloc, 2, size_t, nmemb, "%u", size_t, size, "%u") \
-	x(void *, realloc, 2, void *, ptr, "%p", size_t, size, "%p") \
-	x(void *, reallocarray, 3, void *, ptr, "%p", size_t, nmemb, "%u", size_t, size, "%u")
-
-#define DEFINE_(ret, name, dec) \
-	ret (*name ## _p)(dec);
-#define DEFINE1(ret, name, type1, name1, fmt1) \
-	DEFINE_(ret, name, CONCAT(type1 name1))
-#define DEFINE2(ret, name, type1, name1, fmt1, type2, name2, fmt2) \
-	DEFINE_(ret, name, CONCAT(type1 name1, type2 name2))
-#define DEFINE3(ret, name, type1, name1, fmt1, type2, name2, fmt2, type3, name3, fmt3) \
-	DEFINE_(ret, name, CONCAT(type1 name1, type2 name2, type3 name3))
-#define DEFINE(ret, name, n, ...) \
-        DEFINE ## n (ret, name, ## __VA_ARGS__)
-MALLOCFN_X(DEFINE)
-#undef DEFINE_
-#undef DEFINE1
-#undef DEFINE2
-#undef DEFINE3
-#undef DEFINE
-
-#define DEFINE_(ret, name, dec, call, fmt) \
-	ret name (dec) { \
-		fprintf(stderr, "%s " fmt "\n", __func__, call); \
-		return (ret)0; /* FIXME implement me */ \
-		return name ## _p(call); \
-	}
-#define DEFINE1(ret, name, type1, name1, fmt1) \
-	DEFINE_(ret, name, CONCAT(type1 name1), CONCAT(name1), #name1 " " fmt1)
-#define DEFINE2(ret, name, type1, name1, fmt1, type2, name2, fmt2) \
-	DEFINE_(ret, name, CONCAT(type1 name1, type2 name2), CONCAT(name1, name2), #name1 " " fmt1 " " #name2 " " fmt2)
-#define DEFINE3(ret, name, type1, name1, fmt1, type2, name2, fmt2, type3, name3, fmt3) \
-	DEFINE_(ret, name, CONCAT(type1 name1, type2 name2, type3 name3), CONCAT(name1, name2, name3), #name1 " " fmt1 #name2 " " fmt2 " " #name3 " " fmt3)
-#define DEFINE(ret, name, n, ...) \
-        DEFINE ## n (ret, name, ## __VA_ARGS__)
-MALLOCFN_X(DEFINE)
-#undef DEFINE_
-#undef DEFINE1
-#undef DEFINE2
-#undef DEFINE3
-#undef DEFINE
-
-void init(void) __attribute__((constructor));
-void init(void) {
-#define INIT(ret, name, n, ...) \
-	name ## _p = dlsym(RTLD_NEXT, # name);
-MALLOCFN_X(INIT)
-#undef INIT
-}
-
-#endif
